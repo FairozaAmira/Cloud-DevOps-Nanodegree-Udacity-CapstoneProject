722cd6eb5756a22bb5483fbc8c4b8ea2488b26ad
Fairoza Amira Binti Hamzah <fairozaamira@gmail.com>
2020-08-25 10:03:26 +0800

    edit docker runfile

--- scripts/upload-docker.sh
+++ scripts/upload-docker.sh
@@ -11,7 +11,7 @@ dockerpath="FairozaAmira/capstone-app"
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
-# docker login --username sabbir33
+docker login --username ubuntu
 docker image tag capstone-app $dockerpath
 
 # Step 3:
